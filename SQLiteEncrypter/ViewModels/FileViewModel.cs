a2df8b4f2ba61269df0b5a3b98ac0ccec55cff22
tanguyhardion <45904983+tanguyhardion@users.noreply.github.com>
2023-04-26 23:17:39 +0200

    File browsing

---
+++ SQLiteEncrypter/ViewModels/FileViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Win32;
+using SQLiteEncrypter.Base;
+
+namespace SQLiteEncrypter.ViewModels
+{
+    public class FileViewModel : ViewModelBase
+    {
+        public RelayCommand BrowseCommand { get; set; }
+        private string _selectedFilePath;
+
+        public FileViewModel()
+        {
+            BrowseCommand = new RelayCommand(_ => OpenDialog());
+        }
+
+        private void OpenDialog()
+        {
+            FileDialog fileDialog = new OpenFileDialog();
+            fileDialog.DefaultExt = ".sqlite";
+            fileDialog.Filter = "SQLite databases (*.sqlite)|*.sqlite | All Files (*.*)|*.*";
+            bool? result = fileDialog.ShowDialog();
+            if (result == true)
+            {
+                this.SelectedFilePath = fileDialog.FileName;
+            }
+        }
+
+        public string SelectedFilePath
+        {
+            get => _selectedFilePath;
+            set
+            {
+                _selectedFilePath = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
---
+++ SQLiteEncrypter/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SQLiteEncrypter.ViewModels
+{
+    public class MainViewModel
+    {
+        private static readonly Lazy<MainViewModel> _instance = new Lazy<MainViewModel>(() => new MainViewModel());
+        public static MainViewModel Instance => _instance.Value;
+
+        public FileViewModel FileViewModel { get; set; }
+
+        private MainViewModel()
+        {
+            FileViewModel = new FileViewModel();
+        }
+    }
+}
