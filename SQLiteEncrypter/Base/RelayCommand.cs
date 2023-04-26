bd1793e3b735afe4fa15da4e658d9e70ce238397
tanguyhardion <45904983+tanguyhardion@users.noreply.github.com>
2023-04-26 23:17:08 +0200

    Base ViewModels

---
+++ SQLiteEncrypter/Base/RelayCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SQLiteEncrypter.Base
+{
+    public class RelayCommand : ICommand
+    {
+        readonly Action<object> _execute = null;
+        readonly Predicate<object> _canExecute = null;
+
+        public RelayCommand(Action<object> execute) : this(execute, null) { }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException("execute");
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute((object)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((object)parameter);
+        }
+    }
+}
