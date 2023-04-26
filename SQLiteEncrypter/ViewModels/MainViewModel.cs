5ffa7e42fdf2c5e56e73b2104318703a5a9fc2c8
tanguyhardion <45904983+tanguyhardion@users.noreply.github.com>
2023-04-26 23:58:01 +0200

    VM grouping

--- SQLiteEncrypter/ViewModels/MainViewModel.cs
+++ SQLiteEncrypter/ViewModels/MainViewModel.cs
@@ -1,17 +1,59 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.Win32;
+using SQLiteEncrypter.Base;
+using System;
 
 namespace SQLiteEncrypter.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : ViewModelBase
     {
         private static readonly Lazy<MainViewModel> _instance = new Lazy<MainViewModel>(() => new MainViewModel());
         public static MainViewModel Instance => _instance.Value;
 
-        public FileViewModel FileViewModel { get; set; }
+        #region FIELDS
+        private string _selectedFilePath;
+        #endregion
 
-        private MainViewModel()
+        #region COMMANDS
+        public RelayCommand BrowseCommand { get; set; }
+        public RelayCommand EncryptCommand { get; set; }
+        #endregion
+        
+        public MainViewModel()
         {
-            FileViewModel = new FileViewModel();
+            BrowseCommand = new RelayCommand(_ => OpenDialog());
+            EncryptCommand = new RelayCommand(_ => EncryptDatabase());
+        }
+
+        private void OpenDialog()
+        {
+            FileDialog fileDialog = new OpenFileDialog();
+            fileDialog.DefaultExt = ".sqlite";
+            fileDialog.Filter = "SQLite databases (*.sqlite)|*.sqlite | All Files (*.*)|*.*";
+            bool? result = fileDialog.ShowDialog();
+            if (result == true)
+            {
+                this.SelectedFilePath = fileDialog.FileName;
+            }
+        }
+
+        private void EncryptDatabase()
+        {
+            var connectionString = new SqliteConnectionStringBuilder(_selectedFilePath)
+            {
+                Mode = SqliteOpenMode.ReadWriteCreate,
+                Password = "123"
+            }.ToString();
+        }
+
+        public string SelectedFilePath
+        {
+            get => _selectedFilePath;
+            set
+            {
+                _selectedFilePath = value;
+                RaisePropertyChanged();
+            }
         }
     }
 }
